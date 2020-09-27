ba11f4b38659a6a545e9abed83395d32dc2b67d5
Najoua Ghrieb <najoua.ghrieb@bdeb.qc.ca>
2020-09-27 17:29:18 -0400

    Ajout de la classe Etudiant.cs

---
+++ Etudiant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubProjet2
+{
+    public class Etudiant
+    {
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public override string ToString()
+        {
+            return "Nom : " + Nom + "Prenom : " + Prenom;
+        }
+    }
+}
